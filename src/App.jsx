ef82241f927989f010f48bb0e5a7343d91dbc6f2
RimaSem <rima.semetaite@gmail.com>
2023-02-09 10:31:46 +0200

    Set up routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,17 @@ import React from "react";
 import Header from "./components/Header";
 import Cart from "./pages/Cart";
 import Photos from "./pages/Photos";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div>
       <Header />
-      <h1>Home Page</h1>
+      <Routes>
+        <Route path="/pic-some/" element={<Photos />} />
+        <Route path="/pic-some/cart" element={<Cart />} />
+      </Routes>
+      ;
     </div>
   );
 }
