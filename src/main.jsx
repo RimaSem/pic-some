d1eeef200113b5999cc1532e92a82396b8d4f529
RimaSem <rima.semetaite@gmail.com>
2023-02-09 10:59:37 +0200

    Create context

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { BrowserRouter as Router } from "react-router-dom";
+import { AppContextProvider } from "./appContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <AppContextProvider>
+      <Router>
+        <App />
+      </Router>
+    </AppContextProvider>
   </React.StrictMode>
 );
