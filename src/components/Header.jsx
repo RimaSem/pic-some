1d936668d54764d04dc0ac999d737cf722ddf92e
RimaSem <rima.semetaite@gmail.com>
2023-02-10 10:33:45 +0200

    Create CartItem component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AppContext } from "../appContext";
 
 function Header() {
+  const { cartItems } = useContext(AppContext);
   return (
     <header>
       <h2>
         <Link to="/pic-some/">Pic Some</Link>
       </h2>
       <Link to="/pic-some/cart">
-        <i className="ri-shopping-cart-line ri-fw ri-2x"></i>
+        <i
+          className={`ri-shopping-cart-${
+            cartItems.length > 0 ? "fill" : "line"
+          } ri-fw ri-2x`}
+        ></i>
       </Link>
     </header>
   );
