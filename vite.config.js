1d2502bd1b48cd330c598c31af977b03174cedb1
RimaSem <rima.semetaite@gmail.com>
2023-02-11 19:06:04 +0200

    Config fix

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/pic-some/",
-  plugins: [react()],
+  plugins: [react({ jsxRuntime: "automatic", fastRefresh: true })],
+  mode: "development",
+  build: {
+    minify: false,
+  },
 });
